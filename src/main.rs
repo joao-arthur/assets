44f18e28c6efe714d474feedad0c6e6a83eed195
joao-arthur <joao.lothamer@gmail.com>
2025-02-12 13:13:36 -0300

    feat: map md files to server

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,19 @@ async fn serve() {
         .route_service("/blog.json", ServeFile::new("./blog.json"))
         .route_service("/portfolio.json", ServeFile::new("./portfolio.json"))
         .route_service("/contents_blog.json", ServeFile::new("./contents_blog.json"))
-        .route_service("/contents_portfolio.json", ServeFile::new("./contents_portfolio.json"));
-
+        .route_service("/contents_portfolio.json", ServeFile::new("./contents_portfolio.json"))
+        .route_service("/blog/html-and-components/en.md", ServeFile::new("./blog/html-and-components/en.md"))
+        .route_service("/blog/html-and-components/pt.md", ServeFile::new("./blog/html-and-components/pt.md"))
+        .route_service("/blog/interfaces/en.md", ServeFile::new("./blog/interfaces/en.md"))
+        .route_service("/blog/interfaces/pt.md", ServeFile::new("./blog/interfaces/pt.md"))
+        .route_service("/blog/layered-architecture-ddd-and-market/en.md", ServeFile::new("./blog/layered-architecture-ddd-and-market/en.md"))
+        .route_service("/blog/layered-architecture-ddd-and-market/pt.md", ServeFile::new("./blog/layered-architecture-ddd-and-market/pt.md"))
+        .route_service("/blog/unix-like-and-packages/en.md", ServeFile::new("./blog/unix-like-and-packages/en.md"))
+        .route_service("/blog/unix-like-and-packages/pt.md", ServeFile::new("./blog/unix-like-and-packages/pt.md"))
+        .route_service("/portfolio/libre_game_of_life/en.md", ServeFile::new("./portfolio/libre_game_of_life/en.md"))
+        .route_service("/portfolio/libre_game_of_life/pt.md", ServeFile::new("./portfolio/libre_game_of_life/pt.md"))
+        .route_service("/portfolio/precise_schedule/en.md", ServeFile::new("./portfolio/precise_schedule/en.md"))
+        .route_service("/portfolio/precise_schedule/pt.md", ServeFile::new("./portfolio/precise_schedule/pt.md"));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3307));
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
